c1eb2fc90e1d3ef558f4e3c78511f07ff4cf3212
Mike Cantelon <mcantelon@gmail.com>
2018-10-23 20:52:44 +0000

    Minor cleanup and created class for Git log style checker.

--- git_log_style_checker.py
+++ git_log_style_checker.py
@@ -1,77 +1,79 @@
-def parse_and_check_git_log_output(log):
-    commits = []
-    commit = {}
-    blank_lines = 0
+class GitLogStyleChecker:
+    def __init__(self, git_log_output):
+        self.commits = self.parse_and_check_git_log_output(git_log_output)
 
-    for line in log.split("\n"):
-        if line.startswith("commit "):
-            # Note errors, append commit, reset bookkeeping variables
-            if commit != {}:
-                check_commit_and_note_style_errors(commit)
-                commits.append(commit)
-                commit = {}
-                blank_lines = 0
-            commit["hash"] = line.replace("commit ", "")
-        elif line.startswith("Author: "):
-            commit["author"] = line.replace("Author: ", "")
-        elif line.startswith("Date:   "):
-            commit["date"] = line.replace("Date:   ", "")
-        elif "body" not in commit and line.strip() != "":
-            commit["header"] = line[4:]
-            commit["body"] = []
-        elif line.strip() != "":
-            # Note if body line appears too early
-            if blank_lines == 1:
-                commit["no blank line before body"] = True
-            commit["body"].append(line[4:])
-        else:
-            blank_lines += 1
+    def parse_and_check_git_log_output(self, log):
+        commits = []
+        commit = {}
+        blank_lines = 0
 
-    if commit != {}:
-        check_commit_and_note_style_errors(commit)
-        commits.append(commit)
+        for line in log.split("\n"):
+            if line.startswith("commit "):
+                # Note errors, append commit, reset bookkeeping variables
+                if commit != {}:
+                    self.check_commit_and_note_style_errors(commit)
+                    commits.append(commit)
+                    commit = {}
+                    blank_lines = 0
+                commit["hash"] = line.replace("commit ", "")
+            elif line.startswith("Author: "):
+                commit["author"] = line.replace("Author: ", "")
+            elif line.startswith("Date:   "):
+                commit["date"] = line.replace("Date:   ", "")
+            elif "body" not in commit and line.strip() != "":
+                commit["header"] = line[4:]
+                commit["body"] = []
+            elif line.strip() != "":
+                # Note if body line appears too early
+                if blank_lines == 1:
+                    commit["no blank line before body"] = True
+                commit["body"].append(line[4:])
+            else:
+                blank_lines += 1
 
-    return commits
+        if commit != {}:
+            self.check_commit_and_note_style_errors(commit)
+            commits.append(commit)
 
+        return commits
 
-def check_commit_and_note_style_errors(commit):
-    errors = []
+    def check_commit_and_note_style_errors(self, commit):
+        errors = []
 
-    # Check header
-    if len(commit["header"]) > 50:
-        errors.append("Subject line exceeds 50 characters.")
+        # Check header
+        if len(commit["header"]) > 50:
+            errors.append("Subject line exceeds 50 characters.")
 
-    # Check if body occurs directly after header
-    if "no blank line before body" in commit:
-        errors.append("If multi-line, needs blank line after subject line.")
+        # Check if body occurs directly after header
+        if "no blank line before body" in commit:
+            errors.append("If multi-line, needs blank line after subject line.")
 
-    # Check lines
-    line_number = 1
-    for line in commit["body"]:
-        if len(line) > 72:
-            errors.append("Body line {} exceeds 72 characters.".format(line_number))
-        line_number += 1
+        # Check lines
+        line_number = 1
+        for line in commit["body"]:
+            if len(line) > 72:
+                errors.append("Body line {} exceeds 72 characters.".format(line_number))
+            line_number += 1
 
-    # Add any errors to commit and append
-    if len(errors):
-        commit["errors"] = errors
+        # Add any errors to commit and append
+        if len(errors):
+            commit["errors"] = errors
 
+    def summarize_style_errors(self):
+        #parsed_log = self.parse_and_check_git_log_output(commit_log_text)
 
-def summarize_style_errors_in_commit_log(commit_log_text):
-    parsed_log = parse_and_check_git_log_output(commit_log_text)
+        summary = ''
 
-    summary = ''
+        for commit in self.commits:
+            if "errors" in commit and len(commit["errors"]):
+                summary += "Style error(s) found in commit {}:\n\n".format(commit["hash"])
 
-    for commit in parsed_log:
-        if "errors" in commit and len(commit["errors"]):
-            summary += "Style error(s) found in commit {}:\n\n".format(commit["hash"])
+                for line in commit["errors"]:
+                    summary += "    {}\n".format(line)
 
-            for line in commit["errors"]:
-                summary += "    {}\n".format(line)
+                summary += "\n"
 
-            summary += "\n"
+        if summary == '':
+            summary += "No style errors found.\n"
 
-    if summary == '':
-        summary += "No style errors found.\n"
-
-    return summary
+        return summary
