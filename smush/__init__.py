45d8acd11bc281c3b7fc366cddfca42c33b7bbf4
Mike Cantelon <mcantelon@gmail.com>
2020-07-22 16:10:21 -0700

    Fix for Python 3.

--- smush/__init__.py
+++ smush/__init__.py
@@ -1,2 +1,9 @@
-from git_log_style_checker import GitLogStyleChecker
-from topicmerge import CurrentRepo, TopicMerge
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
+from smush.git_log_style_checker import GitLogStyleChecker
+from smush.topicmerge import CurrentRepo, TopicMerge
+
+__version__ = "0.0.1"
