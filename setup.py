9f7153e3f3fd4ffe4776cff297db2a59a1cf4dbc
Mike Cantelon <mcantelon@gmail.com>
2020-07-29 15:27:28 -0700

    Setup.py cleanup.

--- setup.py
+++ setup.py
@@ -1,35 +1,33 @@
 import os
 from setuptools import setup
 
-# The directory containing this file
-HERE = os.path.dirname(os.path.realpath(__file__))
+# Get requirements
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-# The text of the README file
-readmeFile = os.path.join(HERE, "README.rst")
-README = open(readmeFile, "r").read()
+# Get text of the README file
+with open('README.rst') as f:
+    README = f.read()
+
 
 setup(
-  name = 'smush',
-  packages = ['smush'],
-  version = '0.0.3',
+  name='smush',
+  packages=['smush'],
+  version='0.0.3',
   license='MIT',
-  description = 'Tool to automate merging of topic branches',
-  author = 'Mike Cantelon',
-  author_email = 'mcantelon@gmail.com',
+  description='Tool to automate merging of topic branches',
+  author='Mike Cantelon',
+  author_email='mcantelon@gmail.com',
 
-  long_description = README,
-  long_description_content_type = "text/x-rst",
+  long_description=README,
+  long_description_content_type='text/x-rst',
 
-  url = 'https://github.com/artefactual-labs/smush',
-  download_url = 'https://github.com/artefactual-labs/smush/archive/v0.0.1.zip',
+  url='https://github.com/artefactual-labs/smush',
+  download_url='https://github.com/artefactual-labs/smush/archive/v0.0.1.zip',
 
-  keywords = ['git'],
+  keywords=['git'],
 
-  install_requires = [
-    "future",
-    "gitpython",
-    "pygithub",
-    "PyYAML",
-  ],
-  scripts = ['bin/smush'],
+  install_requires=requirements,
+
+  scripts=['bin/smush'],
 )
