1a96637f9b16aee942846edb884ca412a1ea7c23
Mike Cantelon <mcantelon@gmail.com>
2020-08-05 02:27:51 -0700

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst') as f:
 setup(
   name='smush',
   packages=['smush'],
-  version='0.0.4',
+  version='0.0.5',
   license='MIT',
   description='Tool to automate merging of topic branches',
   author='Mike Cantelon',
